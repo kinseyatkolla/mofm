6db7c62e805eaf483e16ed2bf90ff3ae6a143617
Kinsey Watts <kinsey@kollacode.com>
2026-01-09 19:25:32 -0700

    favicon bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-})
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+  publicDir: "public",
+});
