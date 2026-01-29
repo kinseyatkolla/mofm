a04bb07629d890d96518f0aaed3b44aa82180b4c
Kinsey Watts <kinsey@kollacode.com>
2026-01-29 11:34:22 -0700

    fixes new dpbp section

--- src/components/sections/DowntownParking.vue
+++ src/components/sections/DowntownParking.vue
@@ -11,39 +11,42 @@
         </p>
       </div>
       <div class="section-content">
-        <li>
-          Implementation of the Downtown Benefit Parking Program:
-          <ul style="margin-left: 2rem; list-style-type: triangle">
-            <li>
-              Enable downtown residential permit holders to park in the City's
-              Canon lot free of charge (permits granted by application)
-            </li>
-            <li>
-              Enable downtown residential permit holders to park on Manitou
-              Avenue, Park Avenue, and Canon Avenue from 5:00 p.m. through 10:00
-              a.m. the following day free of charge in areas that are not
-              already designated as RPP zones
-            </li>
-            <li>
-              Allow employees of downtown businesses to obtain permits allowing
-              them to park in the Canon lot free of charge from 5:00 p.m.
-              through 10:00 a.m. the following day
-            </li>
-            <li>
-              Allow employees to purchase permits to park in the Canon lot
-              between the hours of 10:00 a.m. and 5:00 p.m. Business employee
-              permits shall be approved based on need by application.
-            </li>
-            <li>
-              Allow for applying the Downtown Benefit Parking Program to the
-              Wichita and Smischny lots
-            </li>
-          </ul>
-        </li>
-        <li>
-          Expansion of reasonably priced, short-term parking for customers of
-          local stores by allowing for 3 hours of standard pricing instead of 2
-        </li>
+        <ul>
+          <li>
+            Implementation of the Downtown Benefit Parking Program:
+            <ul style="margin-left: 2rem">
+              <li>
+                Enable downtown residential permit holders to park in the City's
+                Canon lot free of charge (permits granted by application)
+              </li>
+              <li>
+                Enable downtown residential permit holders to park on Manitou
+                Avenue, Park Avenue, and Canon Avenue from 5:00 p.m. through
+                10:00 a.m. the following day free of charge in areas that are
+                not already designated as RPP zones
+              </li>
+              <li>
+                Allow employees of downtown businesses to obtain permits
+                allowing them to park in the Canon lot free of charge from 5:00
+                p.m. through 10:00 a.m. the following day
+              </li>
+              <li>
+                Allow employees to purchase permits to park in the Canon lot
+                between the hours of 10:00 a.m. and 5:00 p.m. Business employee
+                permits shall be approved based on need by application.
+              </li>
+              <li>
+                Allow for applying the Downtown Benefit Parking Program to the
+                Wichita and Smischny lots
+              </li>
+            </ul>
+          </li>
+          <li>
+            Expansion of reasonably priced, short-term parking for customers of
+            local stores by allowing for 3 hours of standard pricing instead of
+            2
+          </li>
+        </ul>
       </div>
     </div>
   </section>
